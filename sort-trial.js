2438302c207953763afb01704c1ba80f6576d21c
Tamil Prabha <me.tamilprabha@gmail.com>
2022-08-22 10:52:43 +0530

    comment for descending

--- sort-trial.js
+++ sort-trial.js
@@ -1,11 +1,15 @@
-// sorting strings
+// sorting strings in ascending order
 
 list = ["orange", "lemon", "apple", "kiwi"];
-const result = list.sort();
+const result = list.sort();   
 console.log(result);
 
-// sorting numbers
+// for descending 
+// list.sort();
+// list.reverse();
+
+// sorting numbers in ascending order
 
 numbers = [25, 34,2,89,56,22];
-const sortResult = numbers.sort(function(a, b){return a-b});
+const sortResult = numbers.sort(function(a, b){return a-b}); // for descending : {return b-a}
 console.log(sortResult);
