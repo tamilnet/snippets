1621cf085e3a2fb6a40b67a306995f90adbfd50e
Tamil Prabha <me.tamilprabha@gmail.com>
2022-08-22 10:46:13 +0530

    sort numbers

--- sort-trial.js
+++ sort-trial.js
@@ -1,4 +1,11 @@
+// sorting strings
+
 list = ["orange", "lemon", "apple", "kiwi"];
-const length = list.length;
 const result = list.sort();
 console.log(result);
+
+// sorting numbers
+
+numbers = [25, 34,2,89,56,22];
+const sortResult = numbers.sort(function(a, b){return a-b});
+console.log(sortResult);
