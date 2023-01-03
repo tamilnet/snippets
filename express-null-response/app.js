0dc9f0a3fd1878dc690d1cd551f5be9cc51ead2b
Arul <me@arulraj.net>
2023-01-03 05:32:12 +0530

    add null response in expressjs

---
+++ express-null-response/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port);
+
+server.timeout = 1000 * 60 * 10; // 10 minutes
+
+// Use middleware to set the default Content-Type
+app.use(function (req, res, next) {
+  res.header('Content-Type', 'application/json');
+  next();
+});
+
+app.get('/api/hello', (req, res) => {
+  res.status(200).json({value: 1});
+})
+
+app.get('/api/hello-null', (req, res) => {
+  res.status(200).json({value: undefined});
+})
