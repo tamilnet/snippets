6440498a3bd0be61b3785422a5268dab6031bfc6
Tamil Prabha <me.tamilprabha@gmail.com>
2023-06-11 19:23:25 +0530

    Spread and rest

--- spreadAndRest.js
+++ spreadAndRest.js
@@ -24,3 +24,13 @@ const filter = (...args) => {
   return args.filter(el => el === 3);
 }
 console.log(filter(1, 2, 3)); // [3]
+
+let assessmentDetails = assessmentResult.assessmentDetails;
+    //   // loop all questions and save the active status questions in a array
+    //   let activeQuestions = [];
+    //   for (l = 0; l < assessmentDetails.length; l++) {
+    //     if(assessmentDetails[l].questionStatus == "ACTIVE"){
+    //       activeQuestions.push(assessmentDetails[l]);
+    //     }
+    //   }
+    //   assessmentResult.assessmentDetails = activeQuestions;
