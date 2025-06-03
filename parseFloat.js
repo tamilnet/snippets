24c9d3bb00b252f1ae807b5f17bdb4c284a8a23f
Tamil Prabha <me.tamilprabha@gmail.com>
2025-06-03 19:38:53 +0530

    parseFloat

---
+++ parseFloat.js
@@ -0,0 +1,10 @@
+let num = "92.5589";
+let parsedNum = parseFloat(num);
+console.log(parsedNum);
+
+
+// toFixed() is used to format a number to a fixed number of decimal places
+
+// let num = "92.5589";
+// let parsedNum = parseFloat(num).toFixed(1);
+// console.log(parsedNum);
